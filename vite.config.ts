ede8a4e57626831e5b11955b459b976645b4687f
tkong <tkong@equaltable.net>
2026-06-23 14:55:16 +0900

    feat: update base path in vite.config.ts for deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/newed-portfolio/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
